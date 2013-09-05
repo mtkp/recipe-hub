4cc17843b9080fe8917619fb3dd77a45f6d786e3
Matt Kipps <matkips@gmail.com>
2013-09-05 12:41:41 -0700

    add basic instruction test

--- test/models/instruction_test.rb
+++ test/models/instruction_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class InstructionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "instruction attr" do
+    instruction = Instruction.new
+    assert instruction.invalid?
+    assert instruction.errors[:body].any?
+    assert instruction.errors[:position].any?
+  end
 end
