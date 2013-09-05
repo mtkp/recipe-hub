121a3faedc1ffcc81787a7399a7f5e942bef7671
Matt Kipps <matkips@gmail.com>
2013-09-05 01:02:52 -0700

    Currently not used, using only email for sign in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,6 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:sign_up) do |u|
         u.permit(:username, :email, :password, :password_confirmation)
       end
-      devise_parameter_sanitizer.for(:sign_in) do |u|
-        u.permit(:username, :email)
-      end
       devise_parameter_sanitizer.for(:account_update) do |u|
         u.permit(:username, :email, :password, :password_confirmation, :current_password)
       end
