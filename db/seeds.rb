ef84b62ea03a22d0020434376fc0d2ba25bfd3a9
Matt Kipps <matkips@gmail.com>
2013-09-05 01:17:20 -0700

    Seed for the db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# start this seed with an empty db
+10.times do |n|
+  User.create(username: "user#{n}",
+              email: "user#{n}@example.com",
+              password: "secret_password",
+              password_confirmation: "secret_password")
+end
+
+User.first(10).each_with_index do |user, i|
+  5.times do |j|
+    recipe = user.recipes.build(title: "Recipe #{i}-#{j}")
+    recipe.save!
+    5.times do |k|
+      recipe.ingredients.build(food: "food #{i}-#{j}-#{k}").save!
+      recipe.instructions.build(body: "Be sure to blah blah blah", position: k).save!
+    end
+  end
+end
