2dc74fe1e18d87df42871e1e68e2fcaad78d6ca4
Matt Kipps <matkips@gmail.com>
2013-09-09 23:03:36 -0700

    "A dash of Ajax" for starring a recipe

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -5,13 +5,19 @@ class StarsController < ApplicationController
   # POST /instructions
   def create
     @star = Star.find_or_create_by(recipe_id: @recipe.id, user_id: current_user.id)
-    redirect_to @recipe, notice: 'Recipe starred!'
+    respond_to do |format|
+      format.html { redirect_to @recipe, notice: 'Recipe starred!' }
+      format.js { render 'reload_partial' }
+    end
   end
 
   def destroy
     @star = current_user.stars.where(recipe_id: @recipe.id).first
     @star.destroy if @star
-    redirect_to @recipe, notice: 'Star removed.'
+    respond_to do |format|
+      format.html { redirect_to @recipe, notice: 'Star removed.' }
+      format.js { render 'reload_partial' }
+    end
   end
 
   def index
