f13104a2a8ec2328209117c523d34e07b6bcb285
Matt Kipps <matkips@gmail.com>
2013-10-04 13:31:00 -0700

    Remove collection and branch links in other objects

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -18,10 +18,6 @@ class Recipe < ActiveRecord::Base
   has_one  :source_recipe, through: :source, source: :source
   delegate :user, to: :source_recipe, prefix: true
 
-  # recipe collections
-  has_one :branch, dependent: :destroy
-  has_one :collection, through: :branch
-
   # validations
   validates :title, :user_id, presence: true
 
