48d37151a1addd8ce8d083b32f2802ee2a23a6bb
Matt Kipps <matkips@gmail.com>
2013-11-07 02:58:19 -0800

    Add some partial caching

--- app/helpers/forks_helper.rb
+++ app/helpers/forks_helper.rb
@@ -1,2 +1,10 @@
 module ForksHelper
+
+  # cache recipe forks list with key
+  def cache_key_for_forks_of(recipe)
+    count = recipe.forks_count
+    most_recently_updated = recipe.forks.maximum(:updated_at).utc.to_s(:number)
+    "#{recipe.id}/#{count}/#{most_recently_updated}"
+  end
+
 end
--- app/helpers/stars_helper.rb
+++ app/helpers/stars_helper.rb
@@ -1,2 +1,10 @@
 module StarsHelper
+
+  # cache stars list with key
+  def cache_key_for_stars_of(object)
+    count = object.stars_count
+    most_recently_updated = object.stars.maximum(:updated_at).utc.to_s(:number)
+    "#{object.class.to_s.downcase}/#{object.id}/#{count}/#{most_recently_updated}"
+  end
+
 end
