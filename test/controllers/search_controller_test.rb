50331966841629a10d7c43b9a61c0eda9c6de216
Matt Kipps <matkips@gmail.com>
2014-01-02 12:38:00 -0800

    Search tests

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class SearchControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @recipe = recipes(:tacos)
+    @user = users(:one)
+    sign_in :user, @user
+  end
+
+  teardown do
+    sign_out :user
+  end
+
+  test "should show search" do
+    get :show
+    assert_response :success
+
+    # should have no results if there are no search terms
+    assert_select '#results .item', 0
+  end
+
+  test "should search with search terms" do
+    get :show, search_terms: @recipe.title
+    assert_response :success
+
+    # should have a result with the title of the recipe
+    assert_select '#results .item a', @recipe.title
+  end
 end
